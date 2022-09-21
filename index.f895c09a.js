1995c3dca1478398452513b75813b504c99beaf4
OlesRezvaniuk <105782635+OlesRezvaniuk@users.noreply.github.com>
2022-09-21 11:42:43 +0000

    Deploying to gh-pages from @ OlesRezvaniuk/practic11@7abc8fcca313d06f058915279385116fdca19608 🚀

---
+++ index.f895c09a.js
@@ -0,0 +1,2 @@
+const e={prevBtn:document.querySelector("#prev-button"),nextBrn:document.querySelector("#next-button"),paginationNumbers:document.querySelector("#pagination-numbers"),paginationList:document.querySelector("#paginated-list")}.paginationList.querySelectorAll("li");Math.ceil(e.length/0);
+//# sourceMappingURL=index.f895c09a.js.map
